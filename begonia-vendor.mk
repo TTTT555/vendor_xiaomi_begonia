f8f2e16cfa9ca66bab8c5394014a2bd31d9a77a3
TTTT555 <albukerko@gmail.com>
2021-01-05 23:02:07 +0000

    begonia: OMX: Use bomb decoder for AVC  * Fix Instagram, WhatsApp (and etc.) video issues.

--- begonia-vendor.mk
+++ begonia-vendor.mk
@@ -1115,6 +1115,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libMtkOmxGsmDec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxGsmDec.so \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libMtkOmxMp3Dec.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxMp3Dec.so \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libMtkOmxVdecEx.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVdecEx.so \
+    vendor/xiaomi/begonia/proprietary/vendor/lib/libMtkOmxVdecEx2.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVdecEx2.so \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libMtkOmxVenc.so:$(TARGET_COPY_OUT_VENDOR)/lib/libMtkOmxVenc.so \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libOpenCL.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOpenCL.so \
     vendor/xiaomi/begonia/proprietary/vendor/lib/libSonyIMX230PdafLibrary.so:$(TARGET_COPY_OUT_VENDOR)/lib/libSonyIMX230PdafLibrary.so \
